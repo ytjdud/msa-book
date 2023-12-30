e4017f9d676994b0617e3cec1fc23b07c5e58326
ytjdud <102743371+ytjdud@users.noreply.github.com>
2023-12-31 02:18:04 +0900

    Chore. Impl Gateway Server (use Spring Cloud Gateway instead of Zuul)

--- src/main/java/com/example/msabook/MsaBookApplication.java
+++ src/main/java/com/example/msabook/MsaBookApplication.java
@@ -1,13 +1,13 @@
-package com.example.msabook;
-
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-
-@SpringBootApplication
-public class MsaBookApplication {
-
-	public static void main(String[] args) {
-		SpringApplication.run(MsaBookApplication.class, args);
-	}
-
-}
+//package com.example.msabook;
+//
+//import org.springframework.boot.SpringApplication;
+//import org.springframework.boot.autoconfigure.SpringBootApplication;
+//
+//@SpringBootApplication
+//public class MsaBookApplication {
+//
+//	public static void main(String[] args) {
+//		SpringApplication.run(MsaBookApplication.class, args);
+//	}
+//
+//}
